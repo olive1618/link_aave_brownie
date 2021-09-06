123b8f9d34301c5bd99f56ad1084b2c7ec07d5aa
Emily Shouppe <emily.shouppe@storicard.com>
2021-09-06 18:36:17 -0400

    Initial commit

---
+++ scripts/get_weth.py
@@ -0,0 +1,18 @@
+from brownie import accounts, config, network, interface
+
+
+def main():
+    """
+    Runs the get_weth function to get WETH
+    """
+    get_weth()
+
+
+def get_weth(account=None):
+    """Mints WETH by depositing ETH."""
+    # add your keystore ID as an argument to this call
+    account = (account if account else accounts.add(config["wallets"]["from_key"])) 
+    weth = interface.WethInterface(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.deposit({"from": account, "value": 1000000000000000000})
+    print("Received 1 WETH")
+    return tx
